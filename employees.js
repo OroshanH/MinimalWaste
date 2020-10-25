fdfbb0528fc586401ec2b2262a766b2b0d4a2733
Oro <Oroshanh@hotmail.com>
2020-10-25 02:02:15 +0200

    123

---
+++ employees.js
@@ -0,0 +1,35 @@
+var indeks = 0;
+var cv1 = document.getElementById("orocv");
+var cv2 = document.getElementById("astridcv");
+var cv3 = document.getElementById("magnuscv");
+var cv4 = document.getElementById("vincentcv");
+
+  
+  
+
+      
+ pic.onclick = function(){
+     cv1.style.right = 5 + '%';
+     cv2.style.right = -30 + '%';
+     cv3.style.right = -30 + '%';
+     cv4.style.right = -30 + '%';
+ }
+  pic2.onclick = function(){
+     cv2.style.right = 5 + '%';
+     cv1.style.right = -30 + '%';
+     cv3.style.right = -30 + '%';
+     cv4.style.right = -30 + '%';
+ } 
+ pic3.onclick = function(){
+     cv3.style.right = 5 + '%';
+     cv2.style.right = -30 + '%';
+     cv1.style.right = -30 + '%';
+     cv4.style.right = -30 + '%';
+ } 
+ pic4.onclick = function(){
+     cv4.style.right = 5 + '%';
+     cv2.style.right = -30 + '%';
+     cv3.style.right = -30 + '%';
+     cv1.style.right = -30 + '%';
+ }
+ 
